cfd7b10b27ac1608745830f41a35557b22a6e7f4
Mansi Sadhwani <70261477+MansiSadhwani@users.noreply.github.com>
2020-09-07 11:14:12 +0530

    Update Activity4_2.java

--- Activity4_2.java
+++ Activity4_2.java
@@ -1,15 +1,17 @@
 package Activity4_2;
 
+
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 
 public class Activity4_2 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException
 	{
 	try {
-		File file = new File("src/Activity4_2/input.txt");
+		File file = new File("C://SDET//SDET.txt");
 		Boolean status = file.createNewFile();
 		if(status)
 		{
@@ -19,14 +21,14 @@ public class Activity4_2 {
 		{
 			System.out.println("File already exist");
 		}
-		File fileUtil = FileUtils.getFile("src/Activity4_2/input.text");
+      File fileUtil = FileUtils.getFile("C://SDET//SDET.txt");
 	
-		System.out.println("Data in file: " + FileUtils.readFileToString(fileUtil, "UTF8"));
+		System.out.println("Data in original file : " + FileUtils.readFileToString(fileUtil, "UTF8"));
 		File destDir = new File("resources");
 		FileUtils.copyFileToDirectory(file, destDir);
-		File newFile = FileUtils.getFile(destDir,"input.txt");
+		File newFile = FileUtils.getFile(destDir,"SDET.txt");
 		String newReadData = FileUtils.readFileToString(newFile, "UTF8");
-		System.out.println("Data in file: "+newReadData);
+		System.out.println("Data in new file: "+newReadData);
 	}
 	catch(Exception e)
 	{
